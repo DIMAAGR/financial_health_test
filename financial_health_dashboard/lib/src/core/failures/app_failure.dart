7fa090728f2799d3a71ab2635f751361b35da531
Italo Matos <italomatossouza@hotmail.com>
2026-04-20 22:39:32 -0300

    refactor(financial_health_dashboard): centralize repository failure handling

--- financial_health_dashboard/lib/src/core/failures/app_failure.dart
+++ financial_health_dashboard/lib/src/core/failures/app_failure.dart
@@ -5,15 +5,11 @@ sealed class AppFailure {
 }
 
 class NetworkFailure extends AppFailure {
-  const NetworkFailure([
-    super.message = 'Não foi possível conectar ao serviço.',
-  ]);
+  const NetworkFailure([super.message = 'Não foi possível conectar ao serviço.']);
 }
 
 class ServerFailure extends AppFailure {
-  const ServerFailure([
-    super.message = 'O serviço retornou uma falha inesperada.',
-  ]);
+  const ServerFailure([super.message = 'O serviço retornou uma falha inesperada.']);
 }
 
 class ValidationFailure extends AppFailure {
@@ -21,25 +17,17 @@ class ValidationFailure extends AppFailure {
 }
 
 class StorageFailure extends AppFailure {
-  const StorageFailure([
-    super.message = 'Não foi possível acessar os dados locais.',
-  ]);
+  const StorageFailure([super.message = 'Não foi possível acessar os dados locais.']);
 }
 
 class ParsingFailure extends AppFailure {
-  const ParsingFailure([
-    super.message = 'Os dados retornaram em um formato inválido.',
-  ]);
+  const ParsingFailure([super.message = 'Os dados retornaram em um formato inválido.']);
 }
 
 class UnknownFailure extends AppFailure {
-  const UnknownFailure([
-    super.message = 'Não foi possível concluir a operação.',
-  ]);
+  const UnknownFailure([super.message = 'Não foi possível concluir a operação.']);
 }
 
 class AmountValueFailure extends AppFailure {
-  const AmountValueFailure([
-    super.message = 'Valor inserido na operação deve ser maior que zero.',
-  ]);
+  const AmountValueFailure([super.message = 'Valor inserido na operação deve ser maior que zero.']);
 }
